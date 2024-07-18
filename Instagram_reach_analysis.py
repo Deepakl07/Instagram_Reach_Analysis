16ac489ee6b12740d1778aea6cbc68391b4006b8
Deepakl07 <136842923+Deepakl07@users.noreply.github.com>
2024-07-18 19:52:10 +0530

    first commit

---
+++ Instagram_reach_analysis.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import PassiveAggressiveRegressor
+
+data = pd.read_csv("/content/drive/MyDrive/Instagram.csv", encoding = 'latin1')
+print(data.head())
+
+
+#distribution of impressions received from home
+
+plt.figure(figsize=(10, 8))
+plt.style.use('fivethirtyeight')
+plt.title("Distribution of Impressions From Home")
+sns.histplot(data['From Home'])
+plt.show()
+
+
+
+plt.figure(figsize=(10, 8))
+plt.title("Distribution of Impressions From Hashtags")
+sns.histplot(data['From Hashtags'])
+plt.show()
+
+
+
+plt.figure(figsize=(10, 8))
+plt.title("Distribution of Impressions From Explore")
+sns.histplot(data['From Explore'])
+plt.show()
+
+
+
+#percentage of impressions we are getting from various sources on Instagram
+
+home = data["From Home"].sum()
+hashtags = data["From Hashtags"].sum()
+explore = data["From Explore"].sum()
+other = data["From Other"].sum()
+
+labels = ['From Home','From Hashtags','From Explore','Other']
+values = [home, hashtags, explore, other]
+
+fig = px.pie(data, values=values, names=labels,
+             title='Impressions on Instagram Posts From Various Sources', hole=0.5)
+fig.show()
+
+
+
+#we can create a wordcloud of the caption column to look at the most used words in the caption of Instagram posts
+
+text = " ".join(i for i in data.Caption)
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
+plt.style.use('classic')
+plt.figure( figsize=(12,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+text = " ".join(i for i in data.Hashtags)
+stopwords = set(STOPWORDS)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
+plt.figure( figsize=(12,10))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+
+
+# the relationship between the number of likes and the number of impressions on Instagram posts
+
+figure = px.scatter(data_frame = data, x="Impressions",
+                    y="Likes", size="Likes", trendline="ols",
+                    title = "Relationship Between Likes and Impressions")
+figure.show()
+
+
+
+figure = px.scatter(data_frame = data, x="Impressions",
+                    y="Comments", size="Comments", trendline="ols",
+                    title = "Relationship Between Comments and Total Impressions")
+figure.show()
+
+
+
+figure = px.scatter(data_frame = data, x="Impressions",
+                    y="Shares", size="Shares", trendline="ols",
+                    title = "Relationship Between Shares and Total Impressions")
+figure.show()
+
+
+
+figure = px.scatter(data_frame = data, x="Impressions",
+                    y="Saves", size="Saves", trendline="ols",
+                    title = "Relationship Between Post Saves and Total Impressions")
+figure.show()
+
+
+
+x = np.array(data[['Likes', 'Saves', 'Comments', 'Shares','Profile Visits', 'Follows']])
+y = np.array(data["Impressions"])
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
+
+
+
+model = PassiveAggressiveRegressor()
+model.fit(xtrain, ytrain)
+model.score(xtest, ytest)
+
+
+
+# Features = [['Likes','Saves', 'Comments', 'Shares', 'Profile Visits', 'Follows']]
+features = np.array([[282.0, 233.0, 4.0, 9.0, 165.0, 54.0]])
+model.predict(features)
+
+
+
+
